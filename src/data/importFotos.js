4c0acc9694a76d6c5c719000f2f3847d4b56c5f5
Davidmartcab <davidmartcab@gmail.com>
2022-05-10 09:31:44 +0200

    Update importFotos.js

--- src/data/importFotos.js
+++ src/data/importFotos.js
@@ -1,4 +1,3 @@
-export { default as patro1 } from '../assets/patro1.svg';
 export { default as ayunt_arganda } from '../assets/ayunt_arganda.svg';
 export { default as fmss } from '../assets/fmss.svg';
 export { default as rfess } from '../assets/rfess.svg';
