86da1c6874ef65830d8db70d01f52dbe43b8ce44
Davidmartcab <davidmartcab@gmail.com>
2022-05-10 09:42:12 +0200

    w

--- src/data/importFotos.js
+++ src/data/importFotos.js
@@ -1,4 +1,4 @@
 export { default as ayunt_arganda } from '../assets/ayunt_arganda.svg';
 export { default as fmss } from '../assets/fmss.svg';
 export { default as rfess } from '../assets/rfess.svg';
-export { default as patro1 } from '../assets/patro1.svg';
+export { default as patro1 } from '../assets/patro1.png';
